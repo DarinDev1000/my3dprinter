46ff4377a8ddb81ae41dc7113074b975b0e4a3af
Darin <darindev1000@gmail.com>
2019-03-23 10:30:26 -0700

    Add octoprint test intent

--- handler.ts
+++ handler.ts
@@ -1,10 +1,45 @@
 import * as Ask from 'ask-sdk';
 import 'source-map-support/register';
 
+import axios from "axios";
+// const constants = require('./constants');
+
+const OctoprintTestIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'OctoprintTestIntent';
+  },
+  handle(handlerInput) {
+    const speechText = "Octoprint Test"
+
+    // console.log("Repeat Variable:  ", handlerInput.requestEnvelope.request.intent.slots.repeatName.value);
+    // console.log("Repeat Variable:  ", handlerInput.requestEnvelope.context);
+    // console.log("Repeat Variable:  ", handlerInput);
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Octoprint Test', speechText)
+      // .reprompt("test reprompt")
+      .getResponse();
+  }
+};
+
+const myErrorHandler = {
+  canHandle(handlerInput, error) {
+    return error.name.startsWith('AskSdk');
+  },
+  handle(handlerInput, error) {
+    return handlerInput.responseBuilder
+      .speak('An error was encountered while handling your request. Try again later')
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+}
+
+// Export lambda handlers
 export const alexa = Ask.SkillBuilders.custom()
-  .addRequestHandlers({
-    canHandle: handlerInput => true,
-    handle: handlerInput =>
-      handlerInput.responseBuilder.speak('Hello world!').getResponse()
-  })
+  .addRequestHandlers(
+    OctoprintTestIntentHandler
+  )
+  .addErrorHandlers(myErrorHandler)
   .lambda();
