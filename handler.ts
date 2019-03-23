c1b4748d3d3459c3499c9ce013f7a1f587d0a4c8
Michael Edwards <michaeledwards@Michaels-MBP.hsd1.ca.comcast.net>
2019-03-23 12:12:57 -0700

    finished Search Newest things

--- handler.ts
+++ handler.ts
@@ -5,6 +5,10 @@ import 'source-map-support/register';
 import axios from "axios";
 // const constants = require('./constants');
 
+const api = {
+  thingiverseUrl: "https://api.thingiverse.com/",
+  thingiverseToken: "a34aca11d88f36b8bdd1793cfeafd2b0"
+}
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -39,25 +43,65 @@ const OctoprintTestIntentHandler = {
 }
 
 
-const SearchIntentHandler = {
+const NewestThingsIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'SearchIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'NewestThingsIntent';
   },
-  handle(handlerInput) {
-    const speechText: string = "Returning the ten newest things on thingiverse";
+  async handle(handlerInput) {
+    // const speechText: string = "Returning the ten newest things on thingiverse";
+    const things = [];
+    let data: any;
+    const options = {
+      method: 'get',
+      url: `${api.thingiverseUrl}/newest?access_token=${api.thingiverseToken}`, // Thingiverse get newest
+    }
 
-    console.log("Repeat Variable:  ", handlerInput.requestEnvelope.request.intent);
+    try {
+      data = await axios(options);
+      // console.log('big response', data);
+      console.log('data', data.data)
+      for (let i = 0; i < 10; i++) {
+        things.push(data.data[i])
+        // console.log('Data', data.data[i])
+      }
+
+      // console.log('Response ', data.data);
+    } catch (e) {
+      console.log(e)
+    }
+    let speechText: string = '';
+    console.log(things);
+    for (let item of things) {
+      speechText +=  ", " + item.name;
+      console.log(typeof item.name)
+      console.log(item.name)
+    }
+    console.log('Speech Text', speechText)
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Repeat', speechText)
+      .speak('Here are the ten newest items from thingiverse ' + speechText)
       // .reprompt("test reprompt")
+      .withSimpleCard('Search Newest Things', speechText)
       .getResponse();
   }
 }
 
 
+const CancelIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'Canceled';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .addAudioPlayerStopDirective()
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
 
 const DefaultHandler = {
   canHandle: handlerInput => true,
@@ -80,11 +124,13 @@ const myErrorHandler = {
   }
 }
 
+
 export const alexa = Ask.SkillBuilders.custom()
   .addRequestHandlers(
-    OctoprintTestIntentHandler,
-    SearchIntentHandler,
     LaunchRequestHandler,
+    OctoprintTestIntentHandler,
+    NewestThingsIntentHandler,
+    CancelIntentHandler,
     DefaultHandler
   )
   .addErrorHandlers(myErrorHandler)
